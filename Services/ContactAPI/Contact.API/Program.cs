ad4b5bd445c4c88744d55625beee0aaf82102b0e
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-12-16 00:20:02 +0300

    Services,Models,Infrastructure

--- Services/ContactAPI/Contact.API/Program.cs
+++ Services/ContactAPI/Contact.API/Program.cs
@@ -1,8 +1,12 @@
+using Contact.API.Infrastructure;
+using Contact.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.WebHost.UseUrls("http://*:9000");
 builder.Services.AddControllers();
+builder.Services.AddScoped<IContactService,ContactService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
