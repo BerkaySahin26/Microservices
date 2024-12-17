d542cbb752c94d44b048b6e9ff9cb49bd54cdd4a
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-12-17 22:04:31 +0300

    Ocelot

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -1,15 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Ocelot yapýlandýrma dosyasýný belirtiyoruz
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
+// Ocelot servisini ekliyoruz
+builder.Services.AddOcelot(builder.Configuration);
+
+// Diðer servisler
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,8 +23,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
+
+await app.UseOcelot();
 
 app.MapControllers();
 
